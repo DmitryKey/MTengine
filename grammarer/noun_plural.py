3e839583b241b8379a9ce377048cf754ea8c418e
DmitryKey <dmitry.lucene@gmail.com>
2015-03-01 22:19:26 +0200

    work for #9 by @ariyatan

---
+++ grammarer/noun_plural.py
@@ -0,0 +1,28 @@
+import csv
+
+reader = csv.reader(open('noun_exceptions.csv'))
+noun_exceptions = {}
+
+for row in reader:
+    key = row[0]
+    noun_exceptions[key] = row[1]
+
+no_plural =  ['money','news']
+
+def plural(noun):
+    if noun in no_plural:
+        form = noun
+    elif noun in noun_exceptions:
+        form = noun_exceptions[noun]
+    elif noun[-2:] in ['ch', 'sh']:
+        form = noun + 'es'
+    elif noun[-1:] in ['s', 'x', 'z']:
+        form = noun + 'es'
+    elif noun[-1:] == 'y':
+        form = noun[:-1] + 'ies'
+    else:
+        form = noun + 's'
+    return form
+
+print plural("bacterium")
+
